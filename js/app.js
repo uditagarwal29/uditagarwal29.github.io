6780b43551e5ecac609b4fe9a8d0f6a12a794f83
uditagarwal29 <57745285+uditagarwal29@users.noreply.github.com>
2022-09-10 20:22:03 +0530

    Bigger navigation button in owl carousel

--- js/app.js
+++ js/app.js
@@ -151,6 +151,7 @@ $(".owl-carousel").owlCarousel({
   loop: false,
   margin: 10,
   nav: true,
+  navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
   responsive: {
     0: {
       items: 1,
